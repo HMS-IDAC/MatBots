d731bc9211916bb178baf94933cb38a6b2acc1d4
Cicconet <MARCELO_CICCONET@HMS.HARVARD.EDU>
2017-08-14 17:36:57 -0400

    ...

--- VolumeAnnotationBot/volumeAnnotationTool.m
+++ VolumeAnnotationBot/volumeAnnotationTool.m
@@ -22,6 +22,7 @@ classdef volumeAnnotationTool < handle
         UpperThreshold
         LowerThresholdSlider
         UpperThresholdSlider
+        Slider
         PlaneX
         PlaneY
         PlaneZ
@@ -115,8 +116,22 @@ classdef volumeAnnotationTool < handle
             tool.Dialog = dialog('WindowStyle', 'normal',...
                                 'Name', 'VolumeAnnotationTool',...
                                 'CloseRequestFcn', @tool.closeTool,...
-                                'Position',[100 100 dwidth 8*dborder+8*cheight]);
+                                'Position',[100 100 dwidth 10*dborder+12*cheight]);
             
+            % pencil/eraser slider
+            uicontrol('Parent',tool.Dialog,'Style','text','String','Pencil/Eraser Size','Position',[dborder+20 8.5*dborder+11*cheight cwidth-20 cheight],'HorizontalAlignment','left');
+            tool.PenSizeText = uicontrol('Parent',tool.Dialog,'Style','text','String','5','Position',[dborder+20+(cwidth-20)/2-25 9*dborder+10*cheight 50 cheight],'HorizontalAlignment','center');
+            uicontrol('Parent',tool.Dialog,'Style','edit','String','10','Position',[dborder+cwidth-50 9*dborder+10*cheight 50 cheight],'HorizontalAlignment','right','Callback',@tool.changeSliderRange,'Tag','sliderMax');
+            uicontrol('Parent',tool.Dialog,'Style','edit','String','1','Position',[dborder+20 9*dborder+10*cheight 50 cheight],'HorizontalAlignment','left','Callback',@tool.changeSliderRange,'Tag','sliderMin');
+            tool.PenSize = 5;
+            tool.Slider = uicontrol('Parent',tool.Dialog,'Style','slider','Min',1,'Max',10,'Value',tool.PenSize,'Position',[dborder+20 9*dborder+9*cheight cwidth-20 cheight],'Callback',@tool.sliderManage,'Tag','pss');
+            addlistener(tool.Slider,'Value','PostSet',@tool.continuousSliderManage);
+                            
+            % erase/draw
+            tool.RadioDraw = uicontrol('Parent',tool.Dialog,'Style','radiobutton','Position',[dborder+20 8*dborder+8*cheight 70 cheight],'String','Draw','Callback',@tool.radioDraw);
+            tool.RadioErase = uicontrol('Parent',tool.Dialog,'Style','radiobutton','Position',[dborder+90 8*dborder+8*cheight 70 cheight],'String','Erase','Callback',@tool.radioErase);
+            tool.RadioDraw.Value = 1;
+                            
             % class popup
             uicontrol('Parent',tool.Dialog,'Style','popupmenu','String',labels,'Position', [dborder+20 7*dborder+7*cheight cwidth-20 cheight],'Callback',@tool.popupManage);
                             
@@ -159,10 +174,38 @@ classdef volumeAnnotationTool < handle
             tool.PlaneZLabel = text(tool.PlaneZ(3,1),tool.PlaneZ(3,2),tool.PlaneZ(3,3),sprintf('z = %d', tool.PlaneIndex{3}));
             
             tool.MouseIsDown = false;
-            tool.PenSize = 5;
-            tool.RadioDraw.Value = 1;
             
 %             uiwait(tool.Dialog)
+        end
+        
+        function changeSliderRange(tool,src,callbackdata)
+            value = str2double(src.String);
+            if strcmp(src.Tag,'sliderMin')
+                tool.Slider.Min = value;
+                tool.Slider.Value = value;
+                tool.PenSize = value;
+                tool.PenSizeText.String = sprintf('%d',value);
+            elseif strcmp(src.Tag,'sliderMax')
+                tool.Slider.Max = value;
+                tool.Slider.Value = value;
+                tool.PenSize = value;
+                tool.PenSizeText.String = sprintf('%d',value);
+            end
+        end
+        
+        function sliderManage(tool,src,~)
+            tool.PenSize = round(src.Value);
+            tool.TransparencyHandle{1}.AlphaData =  0.5*reshape(tool.LabelMasks(tool.PlaneIndex{1},:,:,tool.LabelIndex),[tool.NPlanes{2} tool.NPlanes{3}]);
+            tool.TransparencyHandle{2}.AlphaData =  0.5*reshape(tool.LabelMasks(:,tool.PlaneIndex{2},:,tool.LabelIndex),[tool.NPlanes{1} tool.NPlanes{3}]);
+            tool.TransparencyHandle{3}.AlphaData =  0.5*tool.LabelMasks(:,:,tool.PlaneIndex{3},tool.LabelIndex);
+        end
+        
+        function radioDraw(tool,src,~)
+            tool.RadioErase.Value = 1-src.Value;
+        end
+        
+        function radioErase(tool,src,~)
+            tool.RadioDraw.Value = 1-src.Value;
         end
         
         function popupManage(tool,src,~)
@@ -189,9 +232,9 @@ classdef volumeAnnotationTool < handle
                     row = round(p(1,2));
                     imageSize = tool.ImageSize{i};
                     if row > ps && row <= imageSize(1)-ps && col > ps && col <= imageSize(2)-ps
+                        [Y,X] = meshgrid(-ps:ps,-ps:ps);
                         switch i
                             case 1
-                                [Y,X] = meshgrid(-ps:ps,-ps:ps);
                                 Curr = tool.LabelMasks(tool.PlaneIndex{1},row-ps:row+ps,col-ps:col+ps,tool.LabelIndex);
                                 Mask = reshape(sqrt(X.^2+Y.^2) < ps,[1 size(Y,1) size(Y,2)]);
                                 if tool.RadioDraw.Value == 1
@@ -202,7 +245,6 @@ classdef volumeAnnotationTool < handle
                                     tool.TransparencyHandle{1}.AlphaData(row-ps:row+ps,col-ps:col+ps) = reshape(min(Curr,0.5*(1-Mask)),size(Y));
                                 end
                             case 2
-                                [Y,X] = meshgrid(-ps:ps,-ps:ps);
                                 Curr = tool.LabelMasks(row-ps:row+ps,tool.PlaneIndex{2},col-ps:col+ps,tool.LabelIndex);
                                 Mask = reshape(sqrt(X.^2+Y.^2) < ps,[size(Y,1) 1 size(Y,2)]);
                                 if tool.RadioDraw.Value == 1
@@ -213,7 +255,6 @@ classdef volumeAnnotationTool < handle
                                     tool.TransparencyHandle{2}.AlphaData(row-ps:row+ps,col-ps:col+ps) = reshape(min(Curr,0.5*(1-Mask)),size(Y));
                                 end
                             case 3
-                                [Y,X] = meshgrid(-ps:ps,-ps:ps);
                                 Curr = tool.LabelMasks(row-ps:row+ps,col-ps:col+ps,tool.PlaneIndex{3},tool.LabelIndex);
                                 Mask = sqrt(X.^2+Y.^2) < ps;
                                 if tool.RadioDraw.Value == 1
@@ -289,6 +330,27 @@ classdef volumeAnnotationTool < handle
                     tool.Axis{3}.Title.String = sprintf('z = %d', tool.PlaneIndex{3});
                     tool.PlaneZLabel.Position = [tool.PlaneZ(3,1),tool.PlaneZ(3,2),tool.PlaneZ(3,3)];
                     tool.PlaneZLabel.String = sprintf('z = %d', tool.PlaneIndex{3});
+                end
+            elseif strcmp(tag,'pss')
+                tool.PenSize = round(callbackdata.AffectedObject.Value);
+                ps = tool.PenSize;
+                tool.PenSizeText.String = sprintf('%d',ps);
+                [Y,X] = meshgrid(-ps:ps,-ps:ps);
+                Mask = sqrt(X.^2+Y.^2) < ps;
+                
+                for i = 1:3
+                    imageSize = tool.ImageSize{i};
+                    r1 = ceil(tool.Axis{i}.YLim(1));
+                    r2 = floor(tool.Axis{i}.YLim(2));
+                    c1 = ceil(tool.Axis{i}.XLim(1));
+                    c2 = floor(tool.Axis{i}.XLim(2));
+                    rM = round(mean(tool.Axis{i}.YLim));
+                    cM = round(mean(tool.Axis{i}.XLim));
+                    tool.TransparencyHandle{i}.AlphaData(max(1,r1):min(imageSize(1),r2),max(1,c1):min(imageSize(2),c2)) = 0;
+                    if r1 >= 1 && r2 <= imageSize(1) && c1 >= 1 && c2 <= imageSize(2) ...
+                            && rM-ps >= 1 && rM+ps <= imageSize(1) && cM-ps >=1 && cM+ps <= imageSize(2)
+                        tool.TransparencyHandle{i}.AlphaData(rM-ps:rM+ps,cM-ps:cM+ps) = Mask;
+                    end
                 end
             end
         end
