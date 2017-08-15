2dbf445592efb1b3607e09e0d5444b4117842c01
Cicconet <MARCELO_CICCONET@HMS.HARVARD.EDU>
2017-08-15 17:40:34 -0400

    ...

--- VolumeAnnotationBot/volumeRead.m
+++ VolumeAnnotationBot/volumeRead.m
@@ -6,13 +6,6 @@ function V = volumeRead(volumePath)
     for k = 1:nPlanes
         fprintf('.')
         VK = BFData{1}{k,1};
-        if isa(VK,'uint8')
-            VK = double(VK)/255;
-        elseif isa(VK,'uint16')
-            VK = double(VK)/65535;
-        else
-            error('unidentified stack class')
-        end
         V(:,:,k) = VK;
     end
     fprintf('\n')
